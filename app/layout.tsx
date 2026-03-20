1e8d3cc046a9a41a4f2841754f7f90aa7d69a5f9
Jack Humphrey <jack.humphrey@xtra.co.nz>
2026-03-21 04:53:40 +1300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./modern-normalize.css";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-=======
-import { StrictMode } from "react";
 import type { Metadata } from "next";
 import { Tilt_Warp, Roboto } from "next/font/google";
 import Header from "./header";
@@ -27,7 +12,6 @@ const tiltWarp = Tilt_Warp({
 
 const roboto = Roboto({
   variable: "--font-roboto",
->>>>>>> 5f4cdbf (Initial commit from Create Next App)
   subsets: ["latin"],
 });
 
@@ -42,20 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-<<<<<<< HEAD
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={`${tiltWarp.variable} ${roboto.variable}`}>
+        <div className="container">
+          <Header />
+          <main className="main">{children}</main>
+        </div>
       </body>
-=======
-      <StrictMode>
-        <body className={`${tiltWarp.variable} ${roboto.variable}`}>
-          <div className="container">
-            <Header />
-            <main className="main">{children}</main>
-          </div>
-        </body>
-      </StrictMode>
->>>>>>> 5f4cdbf (Initial commit from Create Next App)
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,5 @@
-<<<<<<< HEAD
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className="container">
-      <header>
-        <h1 className="header">
-          <svg viewBox="0 0 500 500">
-            <defs>
-              <path
-                id="circle"
-                d="
-                M 250,250
-                m -125,0
-                a 125,125 0 0 1 250,0 
-                a 125,125 0 0 1 -250,0
-                "
-              />
-            </defs>
-
-            <text textAnchor="start" className="header__text">
-              <textPath href="#circle">Jack Humphrey</textPath>
-            </text>
-          </svg>
-        </h1>
-        <nav className="nav">
-          <Link className="nav__link" href="/">
-            About
-          </Link>
-          <Link className="nav__link" href="/work">
-            Work
-          </Link>
-          <Link className="nav__link" href="/contact">
-            Contact
-          </Link>
-        </nav>
-      </header>
-      <main>Descripton</main>
-    </div>
-  );
-=======
 "use client";
 
 export default function Page() {
   return "Under construction";
->>>>>>> 5f4cdbf (Initial commit from Create Next App)
 }
